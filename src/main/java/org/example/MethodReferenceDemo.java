6170f76db61fba3927f444ba257b02f30cd03dd3
Ilias Papanikolaou <eliaspapanikolaou@hotmail.com>
2023-01-09 16:02:41 +0200

    Init commit

--- src/main/java/org/example/MethodReferenceDemo.java
+++ src/main/java/org/example/MethodReferenceDemo.java
@@ -1,11 +1,8 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class MethodReferenceDemo {
 
